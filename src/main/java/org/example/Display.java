1c4edfe96956dd1a92176df7dee2bda4c9ffeca4
Hamsa Korny <hamsak.@MacBook-Air-von-Hamsa.local>
2024-06-20 14:46:43 +0200

    add display for operations

---
+++ src/main/java/org/example/Display.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Display {
+    public void showInput(String input) {
+        System.out.println("Input: " + input);
+    }
+
+    public void showResult(double result) {
+        System.out.println("Result: " + result);
+    }
+
+    public void showError(String message) {
+        System.out.println("Error: " + message);
+    }
+
+    public void showHistory(String history) {
+        System.out.println("History: \n" + history);
+    }
+
+
+}
